b8614079e41cd16749e51034e0809ad91b908ef6
Hari Iswahyoedi M <haryis@yahoo.com>
2023-12-30 23:29:00 +0700

    Add Script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# build image:tag
+docker build -t ghcr.io/haryis/karsajobs:latest .
+
+# login to Github Container Registry using Personal Access Token
+echo $CR_PAT | docker login ghcr.io -u haryis --password-stdin
+
+# push image to Github Container Registry
+docker push ghcr.io/haryis/karsajobs:latest
